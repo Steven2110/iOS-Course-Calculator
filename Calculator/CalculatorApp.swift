683511752a6d96f60bc8947a0db75897409c0fce
Steven2110 <steven.wijaya2001@gmail.com>
2022-02-11 16:11:06 +0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Steven Wijaya on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
